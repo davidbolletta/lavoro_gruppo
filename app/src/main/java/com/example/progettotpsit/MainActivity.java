8b4597c1e4a8be203de8877c833ef300549029c9
david <bolletta.david@gmail.com>
2021-12-03 12:26:46 +0100

    3 commit

--- app/src/main/java/com/example/progettotpsit/MainActivity.java
+++ app/src/main/java/com/example/progettotpsit/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.progettotpsit;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button butclic = findViewById(R.id.ciprianibutton);
+       
+
+
     }
 }
